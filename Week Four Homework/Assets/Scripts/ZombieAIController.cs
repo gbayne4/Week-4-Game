c1edd6acc11158091bf080a428217953e4149524
gillian <98335596+gbayne4@users.noreply.github.com>
2024-02-20 23:13:27 -0500

    Wanted to cite something

--- Week Four Homework/Assets/Scripts/ZombieAIController.cs
+++ Week Four Homework/Assets/Scripts/ZombieAIController.cs
@@ -17,7 +17,8 @@ namespace HomeworkFour
 
             zombie.SetDestination(player.transform.position);
         }
-
+//was struggling a bit at first since my first attempt with path nav failed so I got some help from this video: https://www.youtube.com/watch?v=K6bBC0qkImI
+        
         // Update is called once per frame
         void Update()
         {
